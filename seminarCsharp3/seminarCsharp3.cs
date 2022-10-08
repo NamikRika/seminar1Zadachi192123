d1a207d7456479466f37e5faaaa161c0283938aa
NamikR <uchitelnamik@mail.ru>
2022-10-08 22:19:25 +0400

    zadacha 23 reshena 2

--- seminarCsharp3/seminarCsharp3.cs
+++ seminarCsharp3/seminarCsharp3.cs
@@ -113,9 +113,9 @@
 // 3 -> 1, 8, 27
 // 5 -> 1, 8, 27, 64, 125
 
-Console.WriteLine("Задача 23 \n Напишите программу, которая принимает на вход число (N) \n и выдаёт таблицу кубов чисел от 1 до N");    
-Console.WriteLine();    
-Console.WriteLine("Решение");
+// Console.WriteLine("Задача 23 \n Напишите программу, которая принимает на вход число (N) \n и выдаёт таблицу кубов чисел от 1 до N");    
+// Console.WriteLine();    
+// Console.WriteLine("Решение");
 
 
 // void tablicaKubov (double chisloN)
