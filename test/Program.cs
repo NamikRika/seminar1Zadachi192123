6b9dee1daad996d269002826244573e6c6070371
NamikR <uchitelnamik@mail.ru>
2022-10-08 19:22:13 +0400

    Zadacha 19 reshena

--- test/Program.cs
+++ test/Program.cs
@@ -11,3 +11,30 @@
 //  Console.WriteLine(Sum(a));
 
 
+// double a = 57675;
+// double z = a / 10000;
+// double x = a / 10000;
+
+// double b  = 0;
+// double c  = 0;
+// b = Math.Truncate(a * 100) / 10;
+
+// 1
+// b = Math.Floor(z);
+// c = a % 10;
+// Console.WriteLine(b);
+// Console.WriteLine(c);
+
+double sd = 56868;
+sd = sd /10000;
+
+Console.WriteLine("test " + sd);
+
+string str = System.Convert.ToString(5.6865);
+string[] parts = str.Split(',');
+Console.WriteLine("Целая часть: " + parts[0]);
+Console.WriteLine("Дробная часть: " + parts[1]);
+
+
+
+
